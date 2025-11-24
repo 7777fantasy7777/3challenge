1331c813bf7f8e3f073e7d2d8798ff64b5a81322
Fantasy <oleksiibohatyrenko2@gmail.com>
2025-11-24 00:09:34 -0800

    update hero imagesw

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -18,10 +18,9 @@ import StarBorder from './StarBorder'
 
 const images = [
   { id: 1, img: "assets/hero-image/hero-image (1).jpg" },
-  // { id: 2, img: "assets/hero-image/hero-image (2).jpg" },
-  // { id: 3, img: "assets/hero-image/hero-image (3).jpg" },
-  // { id: 4, img: "assets/hero-image/hero-image (5).jpg" },
-  // { id: 5, img: "assets/hero-image/hero-image (4).jpg" },
+  { id: 2, img: "assets/hero-image/hero-image (4).jpg" },
+  { id: 3, img: "assets/hero-image/hero-image (3).jpg" },
+  { id: 4, img: "assets/hero-image/hero-image (2).jpg" },
 ];
 
 export const HeroSection = () => {
